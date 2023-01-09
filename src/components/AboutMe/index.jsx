147e3e99c1b4f100e2b79a9bb2ccd0ba09836562
robrodric <74602445+robrodric@users.noreply.github.com>
2023-01-08 22:17:55 -0300

    Update index.jsx

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -13,6 +13,7 @@ function AboutMe() {
         <h2>Informações</h2>
         <h4>Meu nome é Roberto, mais conhecido como robrodric.</h4>
         <h4>Tenho 16 anos, e espero ter um futuro incrível.</h4>
+        <h4>Nas horas vagas faço bots e sites</h4>
       </div>
       <div className="box">
         <h2>Curiosidades</h2>
